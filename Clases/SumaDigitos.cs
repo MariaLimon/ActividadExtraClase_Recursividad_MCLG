07f85c6e279cb39583f9c7bc44e07a0bff8f1555
MariaLimon <limon185mari@gmail.com>
2023-09-18 14:59:46 -0700

    Agregar archivos de proyecto.

---
+++ Clases/SumaDigitos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadExtraClase_Recursividad_MCLG.Clases
+{
+    internal class Recursivo
+    {
+        public int SumaDigitos(int numero)
+        {
+            return;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using ActividadExtraClase_Recursividad_MCLG.Clases;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dame un numero");
+            int numero =  Convert.ToInt16(Console.ReadLine());
+            Recursivo recursividad = new Recursivo();
+            Console.WriteLine(recursividad.SumaDigitos(numero));
+        }
+    }
+}
